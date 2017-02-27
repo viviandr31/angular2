ab89077654815e9980f19deff22823a9256bf150
Jeffrey Liu <jeffrey@Jeffreys-MacBook-Pro.local>
2017-02-26 23:58:06 -0800

    moved unused

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,9 @@
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
-var api = require('./routes/api');
+var api_router = require('./routes/api');
 
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -23,7 +19,7 @@ app.use(function(req, res, next) {
   next();
 });
 
-app.use('/api/', api);
+app.use('/api/', api_router);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
